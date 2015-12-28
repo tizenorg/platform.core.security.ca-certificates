35818b145950d89d8be62f0c97bdff6b02f0e133
sangwan.kwon <sangwan.kwon@samsung.com>
2015-12-28 14:50:36 +0900

    Release initial root CA certs resource
    Change-Id: I7accfa1efd3af6ceedd45b3ce7c20c9dd6c8b734
    Signed-off-by: sangwan.kwon <sangwan.kwon@samsung.com>

---
+++ concat-cacerts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SRC_PATH=$1
+CRT_PATH=$2
+
+if [ -s $CRT_PATH ]
+then
+    rm $CRT_PATH
+fi
+
+for i in `find $SRC_PATH -maxdepth 1 -type f`
+do
+    openssl x509 -in $i -outform PEM >> $CRT_PATH
+done
+
